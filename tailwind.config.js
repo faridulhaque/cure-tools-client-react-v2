e9133a53bff1794125b60a3d28a5bcad8117f663
faridulhaque <faridulhaquemurshed@gmail.com>
2022-06-25 14:31:58 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "rgb(229,76,88)",
+          secondary: "rgb(69,160,163)",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+
+  plugins: [require("daisyui")],
+};
